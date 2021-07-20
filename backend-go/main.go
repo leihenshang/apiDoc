010c5ff965fa47fc789684911e5e483757b070e2
唐志强 <t1339803847@163.com>
2021-07-20 11:09:24 +0800

    添加gin运行模式判断

--- backend-go/main.go
+++ backend-go/main.go
@@ -14,10 +14,15 @@ import (
 
 func main() {
 	fmt.Println("开启api-doc重构第一个版本")
+
+	//设置运行模式
+	if global.MyConf.App.IsRelease() {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	r := gin.New()
 	//初始化路由
 	route.InitRoute(r)
-
 	s := &http.Server{
 		Addr:           ":" + strconv.Itoa(global.MyConf.App.Port),
 		Handler:        r,

9ec12b41040c56ce160cc519d1281757b8934626
唐志强 <t1339803847@163.com>
2021-07-20 10:57:43 +0800

    新增目录

--- backend-go/main.go
+++ backend-go/main.go
@@ -5,15 +5,26 @@ import (
 	_ "fast-duck/goApiDoc/global"
 	"fast-duck/goApiDoc/route"
 	"fmt"
+	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("开启api-doc重构第一个版本")
-	r := gin.Default()
+	r := gin.New()
 	//初始化路由
 	route.InitRoute(r)
-	r.Run(":" + strconv.Itoa(global.MyConf.App.Port))
+
+	s := &http.Server{
+		Addr:           ":" + strconv.Itoa(global.MyConf.App.Port),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+
 }
